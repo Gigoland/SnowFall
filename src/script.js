f560cb070765518c5a132402cd10642d7742124e
GigolDev <gigolnft@gmail.com>
2023-12-28 20:23:39 +0100

    Wip

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 const snowFall = () => {
-	let $container = document.querySelector('.snowfall');
-	for (let i = 0; i < 200; i++) {
-		let $div = document.createElement('div');
-		$div.classList.add('snowflake');
-		$container.appendChild($div)
-	}
+  let $container = document.querySelector('.snowfall');
+  for (let i = 0; i < 200; i++) {
+    let $div = document.createElement('div');
+    $div.classList.add('snowflake');
+    $container.appendChild($div)
+  }
 };
