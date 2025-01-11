fb9d474a6a875c325697dd2ec66ef80976d707cc
GigolDev <gigolnft@gmail.com>
2025-01-12 00:59:31 +0100

    Set time out

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
+/*!
+ * Snowfall v0.0.1 beta
+ * https://www.Gigoland.com
+ * Released under the MIT License
+ */
 const snowFall = () => {
   let $container = document.querySelector('.snowfall');
-  for (let i = 0; i < 200; i++) {
-    let $div = document.createElement('div');
-    $div.classList.add('snowflake');
-    $container.appendChild($div)
-  }
+  setTimeout(() => {
+    for (let i = 0; i < 200; i++) {
+      let $div = document.createElement('div');
+      $div.classList.add('snowflake');
+      $container.appendChild($div)
+    }
+  }, 100);
 };
